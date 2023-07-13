3ddbe3c522c9dc2720285264563b280e1039f7cb
Marcio <marcio1961.ma@gmail.com>
2023-07-13 20:13:24 +0100

    Add styles to questionCard

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import { AnswerObject } from "../App";
 
+import { Wrapper, ButtonWrapper } from "./QuestionsCard.styles";
+
 //definir os tipos das props para este componente
 type Props = {
   question: string;
@@ -22,14 +24,20 @@ const QuestionCard: React.FC<Props> = ({
   totalQuestions,
 }) => (
   //Como estamos a usar tsx quando quisermos pegar expressões de react colocamos o codigo dentro de chavetas
-  <div>
+
+  <Wrapper>
     <p className="number">
       Question: {questionNr}/{totalQuestions}
     </p>
     <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
+      {/* na props coorect esta a ser usado optional chaining,logo se a prop userAnswer  estiver vazia então vai retornar undefined ou  null em vez de erro */}
       {answers.map((answer) => (
-        <div key={answer}>
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
           <button
             disabled={userAnswer ? true : false}
             value={answer}
@@ -37,10 +45,10 @@ const QuestionCard: React.FC<Props> = ({
           >
             <span dangerouslySetInnerHTML={{ __html: answer }}></span>
           </button>
-        </div>
+        </ButtonWrapper>
       ))}
     </div>
-  </div>
+  </Wrapper>
 );
 
 export default QuestionCard;
